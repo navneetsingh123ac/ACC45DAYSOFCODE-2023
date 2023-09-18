777a6de93ce4cf2e75b0eaed15382e0598536f12
Navneet Singh <105372234+navneetsingh123ac@users.noreply.github.com>
2023-09-18 19:09:12 +0530

    Day1
    this is day1 code

---
+++ Day1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Age");
+        int a = sc.nextInt();
+        if (a >= 18) {
+            System.out.println("aap vote kar sakte hai");
+        }
+
+        else {
+            System.out.println("aap vote nahi kar sakte");
+        }
+
+    }
+}
